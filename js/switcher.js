23363dc99341f7d45eca515b195745ad5f22a3cc
Richard Key <rich@busyrich.com>
2019-04-27 19:26:46 -0700

    Added basic mobile support for the case spinner functionality

--- js/switcher.js
+++ js/switcher.js
@@ -2,7 +2,7 @@ const Switcher = function(container, startingIndex) {
   
   this.container = (typeof container === 'string' ? 
     document.getElementById(containerId) : container);
-    
+
   this.images = this.container.querySelectorAll('.image');
   
   this.currentIndex = startingIndex || 0;
@@ -12,28 +12,32 @@ const Switcher = function(container, startingIndex) {
   
   // Set up a few mouse events
   this.container.onmousedown = () => {
+    console.log('START!');
+
     this.dragging = true;
-    this.dragXZero = event.clientX;
+    this.dragXZero = this.getClientX(event);
     this.container.style.cursor = 'grabbing';
   };
 
+  this.container.addEventListener('touchstart', this.container.onmousedown, false);
+
   // Bind this to the document so mouseup anywhere resets dragging
-  document.onmouseup = () => {
+  document.onmouseup = document.ontouchend = () => {
     this.dragging = false;
     this.container.style.cursor = 'grab';
   };
-  
+
   this.container.onmousemove = event => {
     if(!this.dragging) {
       return;
     }
 
-    const currentX = event.clientX;
+    const currentX = this.getClientX(event);
 
     let change = 0;
-    if(this.dragXZero - event.clientX <= -(this.switchPoint)) {
+    if(this.dragXZero - currentX <= -(this.switchPoint)) {
       change = -1;
-    } else if(this.dragXZero - event.clientX >= this.switchPoint) {
+    } else if(this.dragXZero - currentX >= this.switchPoint) {
       change = 1;
     }
 
@@ -42,6 +46,8 @@ const Switcher = function(container, startingIndex) {
       this.dragXZero = currentX;
     }
   };
+
+  this.container.addEventListener('touchmove', this.container.onmousemove, false);
   
   // Set the container's starting cursor
   this.container.style.cursor = 'grab';
@@ -53,6 +59,11 @@ const Switcher = function(container, startingIndex) {
   // Show the current image, IE no offset.
   this.switchImage(0);
 };
+
+Switcher.prototype.getClientX = function(event) {
+  return (event.touches && event.touches.length ? 
+    event.touches[0].clientX : event.clientX);
+}
 
 Switcher.prototype.switchImage = function(indexOffset) {
   this.images[this.currentIndex].style.display = 'none';
