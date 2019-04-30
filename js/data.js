1c3cb81a4b493d526764873dfd8e7895cca5f215
Richard Key <rich@busyrich.com>
2019-04-30 11:47:00 -0700

    New movie and a few new features/info blocks to the site itself.

--- js/data.js
+++ js/data.js
@@ -1,6 +1,26 @@
 const movieData = [
   {
+    "showingDate": "2019-05-06T19:00:00",
+    "imdb": "tt0103621",
+    "runningTime": 90,
+    "commentary": "This is a movie I used to rent with a fury, never realizing how esoteric it was. I never actually owned a copy until a couple years ago when I started looking for it again. This German DVD is the only DVD I could find, but thankfully it has an English audio track. Its super cheesy and super nostalgic. Should be a really fun watch.",
+    "title": "Adventures in Dinosaur City",
+    "backText": [
+      "(direct translation from German) Timmy, Jamie, and Mick are fond of dinosaurs and everything that has to do with it. That's why they love the animated series Dinowars about everything. Because the parents of Timmy, two ingenious chaotic researchers, are attending a congress, the three teenagers take the opportunity to watch dinowars on the big screen of their parental lab ... that has unimaginable consequences. The invention of Timmy's parents beams the three into the world of dinowars. There are great, fantastic adventures and all the heroes of Dinowars waiting for you.",
+    ],
+    "format": "DVD",
+    "imagesLocation": "img/dino_city",
+    "images": [
+      "front.jpg",
+      "right.jpg",
+      "back.jpg",
+      "left.jpg"
+    ]
+  },
+  {
     "showingDate": "2019-04-29T19:00:00",
+    "imdb": "tt0387129",
+    "runningTime": 85,
     "commentary": "The best way I can describe this movie is \"a Christian musical action-drama\". It is probably one of the worst movies I have ever seen, and trust me, I have watched a lot of bad movies. Its bound to make us suffer together, but that is kind of the point of the movie night.",
     "title": "Commander Kellie and the Superkids: The Sword",
     "backText": [
--- js/page.js
+++ js/page.js
@@ -3,33 +3,73 @@ moment.tz.setDefault('America/Phoenix');
 const elms = {
   showtime: document.querySelector('#showtime span'),
   movieTitle: document.getElementById('movieTitle'),
+  imdbLink: document.getElementById('imdb'),
   caseImages: document.getElementById('mediaCase'),
+  runningTime: document.getElementById('runningTime'),
   commentary: document.getElementById('commentary'),
-  description: document.getElementById('description')
+  description: document.getElementById('description'),
+  previousMovies: document.getElementById('previous')
 };
 
-const movie = movieData[0];
+const emptyElm = function(elmName) {
+  const elm = elms[elmName];
+  console.log(elm);
 
-elms.showtime.innerHTML = moment(movie.showingDate).local().calendar();
-elms.movieTitle.innerHTML = movie.title;
-elms.commentary.innerHTML = movie.commentary;
+  if(elm) {
+    while (elm.firstChild) {
+      elm.removeChild(elm.firstChild);
+    }
+  }  
+}
 
-movie.backText.forEach(p => {
-  const backTextP = document.createElement('p');
-  backTextP.innerHTML = p;
+const clearPage = function() {
+  emptyElm('caseImages');
+  emptyElm('description');
+}
 
-  elms.description.appendChild(backTextP);
-});
+const load = function(movieIndex) {
+  //clear out current elements
+  clearPage();
 
-// Add the case images
-movie.images.forEach(img => {
+  const movie = movieData[movieIndex];
+
+  elms.showtime.innerHTML = moment(movie.showingDate).local().calendar();
+  elms.movieTitle.innerHTML = movie.title;
+  elms.imdbLink.href = 'https://www.imdb.com/title/' + movie.imdb;
+  elms.runningTime.innerHTML = movie.runningTime;
+  elms.commentary.innerHTML = movie.commentary;
+
+  movie.backText.forEach(p => {
+    const backTextP = document.createElement('p');
+    backTextP.innerHTML = p;
+
+    elms.description.appendChild(backTextP);
+  });
+
+  // Add the case images
+  movie.images.forEach(img => {
+    const imgElm = document.createElement('img');
+    imgElm.src = movie.imagesLocation + '/' + img;
+    imgElm.classList.add('image');
+    imgElm.draggable = false;
+    
+    elms.caseImages.appendChild(imgElm);
+  });
+
+  // Set up the element switcher with the case images
+  new Switcher(elms.caseImages);
+
+  // Scroll to the top of the page
+  window.scrollTo(0, 0);
+}
+
+load(0);
+
+for(let m = 0; m < movieData.length; m++) {
   const imgElm = document.createElement('img');
-  imgElm.src = movie.imagesLocation + '/' + img;
-  imgElm.classList.add('image');
-  imgElm.draggable = false;
-  
-  elms.caseImages.appendChild(imgElm);
-});
+  imgElm.src = movieData[m].imagesLocation + '/' + movieData[m].images[0];
+  imgElm.onclick = () => load(m);
 
-// Set up the element switcher with the case images
-const s = new Switcher(elms.caseImages);
+  elms.previousMovies.appendChild(imgElm)
+}
+
